b98b864ba85d5a09df73bed3512bbe2302d84e56
JerryR7 <vp780412@gmail.com>
2014-11-10 20:39:59 +0800

    Hide URL index.php
    1. To start shopping cart model

---
+++ application/models/shopping_cart_model.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Shopping_cart_model extends CI_Model
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	
+}
